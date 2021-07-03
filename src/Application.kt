c19ffaee30a6d12bd83908ba81e7b25147504cf5
coda1997 <overlooker1997@gmail.com>
2021-07-03 23:01:27 +0800

    add adding features router

--- src/Application.kt
+++ src/Application.kt
@@ -6,6 +6,7 @@ import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
@@ -19,22 +20,22 @@ fun main(args: Array<String>) {
     val file = FileReader(File("config.json")).readText()
     val config = Gson().fromJson<Dconfig>(file, Dconfig::class.java)
     println(config.toString())
-    connection =  PostgreSQLConnectionBuilder.createConnectionPool(
-            "jdbc:postgresql://${config.ip}:${config.dataBasePort}/${config.dataBaseName}?user=${config.username}&password=${config.password}"
-            )
+    connection = PostgreSQLConnectionBuilder.createConnectionPool(
+        "jdbc:postgresql://${config.ip}:${config.dataBasePort}/${config.dataBaseName}?user=${config.username}&password=${config.password}"
+    )
     connection.connect()
-    embeddedServer(Netty, port = config.port){
+    embeddedServer(Netty, port = config.port) {
         charset("utf-8")
-        install(ContentNegotiation){
+        install(ContentNegotiation) {
             gson {
                 setPrettyPrinting()
             }
         }
-        install(Compression){
+        install(Compression) {
             gzip()
         }
         routing {
-            route("/point"){
+            route("/point") {
                 get {
                     call.respond(findPoint())
                 }
@@ -45,10 +46,87 @@ fun main(args: Array<String>) {
                     val res = addManyPoint(points)
                     call.respondText("insert status: $res")
                 }
-                get("{id}"){
-                    val id = call.parameters["id"]?.toInt()?:0
+                get("{id}") {
+                    val id = call.parameters["id"]?.toInt() ?: 0
                     val res = findPoint(id)
                     call.respond(res)
+                }
+            }
+            route("/feature") {
+                post("/wifi/{bid}/{fid}") {
+                    //handle adding wifi feature
+                    val bid = call.parameters["bid"] ?: ""
+                    val fid = call.parameters["fid"] ?: ""
+                    val multipart = call.receiveMultipart()
+                    multipart.forEachPart { part ->
+                        if (part is PartData.FileItem) {
+                            val name = part.originalFileName!!
+                            val f = File("/uploads/$bid/")
+                            part.streamProvider().use { inputSteam ->
+                                f.outputStream().buffered().use {
+                                    inputSteam.copyTo(it)
+                                }
+                            }
+                            updateWifiFeature(bid, fid, f)
+                        }
+                        part.dispose()
+
+                    }
+                }
+                post("/ble/{bid}") {
+                    //handle adding ble
+                    val bid = call.parameters["bid"] ?: ""
+                    val multipart = call.receiveMultipart()
+                    multipart.forEachPart { part->
+                        if (part is PartData.FileItem){
+                            val name = part.originalFileName!!
+                            val f = File("/uploads/$bid/")
+                            part.streamProvider().use { inputSteam ->
+                                f.outputStream().buffered().use {
+                                    inputSteam.copyTo(it)
+                                }
+                            }
+                            updateBleFeature(bid, f)
+                        }
+                        part.dispose()
+                    }
+                }
+
+                post("/pic/{bid}") {
+                    val bid = call.parameters["bid"] ?: ""
+                    val multipart = call.receiveMultipart()
+                    multipart.forEachPart { part->
+                        if (part is PartData.FileItem){
+                            val name = part.originalFileName!!
+                            val f = File("/uploads/$bid/")
+                            part.streamProvider().use { inputSteam ->
+                                f.outputStream().buffered().use {
+                                    inputSteam.copyTo(it)
+                                }
+                            }
+                            updatePicFeature(bid, f)
+                        }
+                        part.dispose()
+                    }
+                }
+                post("mag/{bid}/{fid}") {
+                    val bid = call.parameters["bid"] ?: ""
+                    val fid = call.parameters["fid"] ?: ""
+                    val multipart = call.receiveMultipart()
+                    multipart.forEachPart { part ->
+                        if (part is PartData.FileItem) {
+                            val name = part.originalFileName!!
+                            val f = File("/uploads/$bid/")
+                            part.streamProvider().use { inputSteam ->
+                                f.outputStream().buffered().use {
+                                    inputSteam.copyTo(it)
+                                }
+                            }
+                            updateMagFeature(bid, fid, f)
+                        }
+                        part.dispose()
+
+                    }
                 }
             }
         }
---
+++ src/FeatureUpdateService.kt
@@ -0,0 +1,20 @@
+package com.dadachen
+
+import java.io.File
+
+fun updateWifiFeature(bid: String, fid: String, file: File): Boolean {
+
+
+    return true
+}
+
+fun updateBleFeature(bid: String, file: File): Boolean {
+    return true
+}
+
+fun updatePicFeature(bid: String, file: File): Boolean{
+    return true
+}
+fun updateMagFeature(bid: String, fid: String, file: File):Boolean{
+    return true
+}
